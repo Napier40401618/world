abd60ee8090b50973d98ded61be1989cb1d45b4e
Napier40401618 <zayyanpaing95@gmail.com>
2018-04-04 10:54:52 +0630

    First commit

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 import json
-page_number = bn,
+
 w= json.load(open("worldl.json"))
 page_size = 10
-
+page_number=0
 app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
-    return render_template('index.html', w = w[0:page_size])
+    return render_template('index.html', w = w[0:page_size],page_size=page_size,page_number=page_number)
 
 @app.route('/begin/<b>')
 def beginpage(b):
     bn = int(b)
     return render_template('index.html',
                            w = w[bn:bn+page_size],
+                           page_number = bn,
                            page_size = page_size
                            )
 
