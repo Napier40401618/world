4e62f01ecc6c7a566e80d71d1b6bca320fe701b1
Napier40401618 <zayyanpaing95@gmail.com>
2018-04-04 10:16:16 +0630

    First commit

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+import json
+page_number = bn,
+w= json.load(open("worldl.json"))
+page_size = 10
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('index.html', w = w[0:page_size])
+
+@app.route('/begin/<b>')
+def beginpage(b):
+    bn = int(b)
+    return render_template('index.html',
+                           w = w[bn:bn+page_size],
+                           page_size = page_size
+                           )
+
+@app.route('/continent/<a>')
+def continentpage(a):
+    cl = [c for c in w if c['continent']==a]
+    return render_template('continent.html', length_of_cl = len(cl), cl = cl,
+                           a = a)
+
+@app.route('/countryByName/<i>')
+def countryByNamepage(i):
+    c = None
+    for x in w:
+        if x['name']== i:
+            c = x
+    return render_template('country.html', c = c)
+
+@app.route('/country/<i>')
+def countrypage(i):
+    return render_template('country.html', c = w[int(i)])
+
+app.run(host='0.0.0.0', port=5618, debug=True)
